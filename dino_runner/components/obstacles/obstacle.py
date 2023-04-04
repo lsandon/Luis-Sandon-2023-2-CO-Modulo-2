97459db46c4db1aa70ac3aac677a0bf00c820d18
Luis Sandon Calderin <luissandon76@gmail.com>
2023-04-03 21:53:46 -0500

    CLASE #3

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -11,8 +11,11 @@ class Obstacle(Sprite):
         self.rect = self.image[obstacle_type].get_rect()
         self.rect.x = SCREEN_WIDTH
 
-    def update(self, game_speed):
+    def update(self, game_speed, obstacles):
         self.rect.x -= game_speed
+        
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
 
     def draw(self, screen):
         screen.blit(self.image[self.obstacle_type], (self.rect.x, self.rect.y))
