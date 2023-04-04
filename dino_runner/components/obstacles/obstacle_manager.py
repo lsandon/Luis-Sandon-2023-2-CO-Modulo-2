7a17051f95a09b755d09af5e19dfe333fb7cc16d
Luis Sandon Calderin <luissandon76@gmail.com>
2023-04-04 03:58:06 -0500

    TAREA CLASE #3 IMPLEMENTATION BIRD

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,28 +1,40 @@
 import pygame
+import random
 
-from dino_runner.components import obstacles
+import pygame
+import random
 
 from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
+
 from dino_runner.utils.constants import SMALL_CACTUS
 
+
 class ObstacleManager:
-    def __init__(self):
-        self.obstacles = []
-
-    def update(self, game):
-        if len(self.obstacles) == 0:
-            cactus = Cactus(SMALL_CACTUS)
-            self.obstacles.append(cactus)
-
-        for obstacle in self.obstacles:
-            obstacle.update(game.game_speed, self.obstacles)
-            if game.player.dino_rect.colliderect(obstacle.rect):
-                game.playing = False
-                break
-            
-            
-
-    def draw(self, screen):
-        for obstacle in self.obstacles:
-            obstacle.draw(screen)
-        
+  def __init__(self):
+    self.obstacles = []
+    
+  def generate_obstacle(self, obstacle_type):
+    if obstacle_type == 0:
+      cactus_type = 'SMALL'
+      obstacle = Cactus(cactus_type)
+    else:
+      obstacle = Bird()
+    return obstacle
+    
+  def update(self, game):
+    if len(self.obstacles) == 0:
+      obstacle_type = random.randint(0, 2)
+      obstacle = self.generate_obstacle(obstacle_type)
+      self.obstacles.append(obstacle)
+      
+    for obstacle in self.obstacles:
+      obstacle.update(game.game_speed, self.obstacles)
+      
+      if game.player.dino_rect.colliderect(obstacle.rect):
+        game.playing = False
+        break
+  
+  def draw(self, screen):
+    for obstacle in self.obstacles:
+      obstacle.draw(screen)
